e25a3490acb3e9ddb7098ac322b4cecbc78f05cc
Vitoria Cezar <91792762+vitoriacm@users.noreply.github.com>
2025-06-15 21:43:17 -0300

    feat(server): allow configuring the port via environment variable

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,8 @@ app.use("/api/daily", dailyRouter);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
